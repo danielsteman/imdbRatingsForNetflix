2b1a8db4760c223c6fe99f50aa407857ccccc4a2
Daniel Steman <daniel-steman@live.nl>
2020-12-28 15:18:41 +0100

    updated query to get netflix movies that have more than 5000 IMDB votes, ordered descending on rating

--- getRecommendations.py
+++ getRecommendations.py
@@ -22,19 +22,21 @@ updateNetflixYears()
 updateOwnRatings()
 
 """
-Query to get ratings for Netflix movies:
+Query to get ratings for Netflix movies that haven't been rated:
 
 SELECT a.title, a.year, b.rating 
 FROM netflix a 
 LEFT JOIN movies_id b 
 ON a.title = b.title AND a.year = b.year 
 WHERE b.type IS 'movie'
-AND b.id NOT IN (SELECT id FROM watched);
+AND b.id NOT IN (SELECT id FROM watched)
+AND b.num > 5000
+ORDER BY b.rating DESC;
 """
 
 con = sqlite3.connect('Database/netimdb.db')
 
-dfOut = pd.read_sql_query("SELECT a.title, a.year, b.rating FROM netflix a LEFT JOIN movies_id b ON a.title = b.title AND a.year = b.year WHERE b.type IS 'movie' AND b.id NOT IN (SELECT id FROM watched)", con)
+dfOut = pd.read_sql_query("SELECT a.title, a.year, b.rating FROM netflix a LEFT JOIN movies_id b ON a.title = b.title AND a.year = b.year WHERE b.type IS 'movie' AND b.id NOT IN (SELECT id FROM watched) AND b.num > 5000 ORDER BY b.rating DESC", con)
 
 con.commit()
 con.close()
